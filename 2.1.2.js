0d4437b0402a30508ae4a336dd0869a41d3ddf98
roby <robortly@163.com>
2019-07-15 20:21:55 +0800

    修改事件

--- 2.1.2.js
+++ 2.1.2.js
@@ -31,3 +31,30 @@ my_async_function("Hello Node.js",(err,name)=>{
     console.log(name);
 });
 
+// class Evente{
+//     constructor(){
+//         this.map={};
+//     }
+//     add(name,fn){
+//         if(this.map[name]){
+//             this.map[name].push(fn);
+//             return;
+//         }
+//     }
+//     emit(name,...args){
+//         this.map[name].forEach(fn=>{
+//             fn(...args);
+//         });
+//     }
+// }
+// let e=new Evente();
+// e.add("hello",(err,name)=>{
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log(name);
+// })
+
+// e.emit("hello","发生了错误");
+// e.emit("hello",null,"hello Nodejs");
