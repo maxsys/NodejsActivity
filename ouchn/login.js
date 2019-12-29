722b30da48f3c587eea9c8bbcc1d273143df96cb
maxsys <gitmax@qq.com>
2019-12-29 18:40:11 +0800

    全局遍历A标记

--- ouchn/login.js
+++ ouchn/login.js
@@ -1,6 +1,22 @@
 //修改弹出窗体
 ///html/body/form/div[4]/ul/li[4]/a
 $x('html/body/form/div[4]/ul/li[4]/a')[0].target='_self';
+
+for (var index = 0; index < document.getElementsByTagName('a').length; index++) {
+    const ea = document.getElementsByTagName('a')[index];
+    //console.log(ea.target);
+    ea.target='_self';
+}
+
+for (var index = 0; index < document.getElementsByTagName('a').length; index++) {
+    console.log(index);
+}
+
+
+document.getElementsByTagName('a').forEach(ea => {
+    console.log(ea.target);
+});
+
 //点击 学生平台：.click();
 $x('html/body/form/div[4]/ul/li[4]/a')[0].click();
 //点击学生登录:/html/body/div/div/div[1]/div/div/div/div/nav/div/div[2]/ul[2]/li[1]/a
